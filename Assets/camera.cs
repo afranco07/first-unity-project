46e76cc40f617ee9b6cc9c488b3b9563ac749670
Alberto Franco <alberto@ghostery.com>
2020-07-03 13:33:46 -0400

    initial commit

---
+++ Assets/camera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+    public Transform followTarget;
+    float distance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        distance = 4;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(followTarget.position.x, followTarget.position.y + 2, followTarget.position.z - distance);
+    }
+}
---
+++ Assets/character.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class character : MonoBehaviour
+{
+    Rigidbody rb;
+    Animator animator;
+    public float jumpForce = 10.0f;
+    public Vector3 jump;
+    bool jumping;
+    float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        jump = new Vector3(0.0f, 0.2f, 0.0f);
+        animator = GetComponent<Animator>();
+        moveSpeed = 5f;
+    }
+
+    void OnCollisionStay() {
+        animator.SetBool("isJumping", false);
+    }
+
+    void OnCollisionExit() {
+        animator.SetBool("isJumping", true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool running = Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0;
+        animator.SetBool("isRunning", running);
+        if (Input.GetKeyDown("space")) {
+            rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+        }
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 vec = new Vector3(horizontalInput, 0f, verticalInput);
+        transform.LookAt(vec + transform.position);
+        transform.Translate(vec * Time.deltaTime * moveSpeed, Space.World);
+    }
+}
